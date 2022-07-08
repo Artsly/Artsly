c764af4a83ad17a096004e5d938d84b64a5721cb
Linux-Gamer <charl.cj.monke@gmail.com>
2022-07-08 14:58:43 +0100

    Update

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import random
 import time
 
 # Lists of prompts
-character = ["Lumbah Jack","Ice Witch","Fire Queen"
-]
+character = ["Lumbah Jack","Ice Witch","Fire Queen"]
 environment = ["Treehouse","Underground bunker"]
 prop = ["Laser gun","Diamond staff"]
 number = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","Unlimited"]
